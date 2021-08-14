cb50a182e7bf2127f6fe684c6ec5810c2f21cd4d
yasirrhaq <yasir.haq98@gmail.com>
2021-08-14 15:07:35 +0800

    Add solutions to algorithms

---
+++ algorithms/ViralAdvertising.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int viralAdvertising(int n) {
+        
+    int shared =5;
+    int liked =0;
+    int result=0;
+    for(int i=0; i<n;i++){
+        liked = (int) Math.floor(shared/2);
+        shared = (int)liked*3;
+        result +=(int)liked;
+    }
+    return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
